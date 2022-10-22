c099f6d106802d60a3d8d2ca247a83ca4e57b434
R. Fariz <farizramazanov38@gmail.com>
2022-10-22 14:22:03 +0400

    feat: add sorting by search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import Categories from "../components/Categories";
 
-const Home = () => {
+const Home = ({searchValue}) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
@@ -20,15 +20,16 @@ const Home = () => {
     const category = categoryId > 0 ? `category=${categoryId}` : "";
     const sortBy = sortType.sortBy.replace('-', '');
     const order = sortType.sortBy.includes('-') ? 'asc' : 'desc';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
-    fetch(`https://634acbd933bb42dca40b7c93.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(`https://634acbd933bb42dca40b7c93.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`)
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue]);
 
   return (
     <div className="container">
